fb121a5ef0ad22203319b82ef4c0432309ceaca2
Maximiliano Zulli <maximiliano.zulli@gmail.com>
2021-03-08 14:25:00 +0100

    Projektdateien hinzufügen.

---
+++ WPFApp2/HomeWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFApp2
+{
+    /// <summary>
+    /// Interaktionslogik für Page1.xaml
+    /// </summary>
+    public partial class HomeWindow : Page
+    {
+        public HomeWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //View Report
+            ReportWindow reportPage = new ReportWindow();
+            this.NavigationService.Navigate(reportPage);
+        }
+    }
+}
