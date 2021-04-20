5403e6a2bef764530eb5dd8db16f5956c1e23ffa
Maximiliano Zulli <maximiliano.zulli@gmail.com>
2021-04-20 10:33:10 +0200

    Added 2 Tests

--- NUnitTestProject1/UnitTest1.cs
+++ NUnitTestProject1/UnitTest1.cs
@@ -39,16 +39,16 @@ namespace NUnitTestProject1
             Assert.IsNotNull(output);
         }
 
-        /*
+        
         [Test]
         public void JSonReadRandTest()
         {
             JObject mapData = JObject.Parse(allText);
-            string output = (string)mapData["route"]["rand"].ToString();
+            string output = (string)mapData["route"]["hasTollRoad"].ToString();
             Assert.IsNotNull(output);
-        }*/
+        }
 
-        [Test]
+        /*[Test]
         public void MakeNewFilenameWithAccendingID()
         {
             DirectoryInfo directory = new DirectoryInfo("..\\..\\..\\..\\WPFApp2\\img");
@@ -68,13 +68,17 @@ namespace NUnitTestProject1
             int intValue = int.Parse(value);
 
             Assert.AreEqual(1, intValue);
-        }
+        }*/
 
         [Test]
         public void TourAddUtilityTest()
         {
             TourAddUtility tourAddUtility = new TourAddUtility();
-
+            tourAddUtility.Description = "Description";
+            tourAddUtility.Name = "Name";
+            Tour tour = new Tour() { Description = tourAddUtility.Description, Name = tourAddUtility.Name };
+            Assert.IsTrue(tour.Name == tourAddUtility.Name && tour.Description == tourAddUtility.Description);
+            
         }
     }
 }
